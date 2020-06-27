739b559f8943c34aedb00b5dd7cecbfa09f2f2d8
melandres8 <melanmosta@gmail.com>
2020-06-27 21:18:57 +0000

    task#7: adding show and create to console file

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """ Create console 0.0.1 """
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +22,36 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """This instruction shouldn't execute anything"""
         pass
+
+    def do_create(self, args):
+        """ Creates a new instance of BaseModel,
+            saves it and print the id.
+        """
+        new_instance = BaseModel()
+        if args == type(new_instance).__name__:
+            new_instance.save()
+            print(new_instance.id)
+        elif not args:
+            print("** class name missing")
+        elif args != type(new_instance).__name__:
+            print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """ Prints the string representation of
+            an instance based an the class name.
+        """
+        arg = args.split(" ")
+        dic = storage.all()
+        if not args:
+            print("** class name missing **")
+        elif arg[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) in dic:
+            print(dic["{}.{}".format(arg[0], arg[1])])
+        else:
+            print("** no instance found **")
 
 
 if __name__ == "__main__":
