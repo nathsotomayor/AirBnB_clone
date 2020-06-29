eb3621cd4a476f72055aec8b78a26b5a4aec1b12
melandres8 <melanmosta@gmail.com>
2020-06-29 20:09:49 +0000

    updating and adding new class

--- console.py
+++ console.py
@@ -2,9 +2,15 @@
 """ Create console 0.0.1 """
 
 import cmd
+import shlex
 from models import storage
+from models.city import City
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.user import User 
 from models.engine.file_storage import FileStorage
 
 
@@ -37,7 +43,7 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """ Creates a new instance of BaseModel,
             saves it and print the id.
-        """ 
+        """
         args = shlex.split(args)
         if args[0] in self.__class_name:
             new_instance = eval(args[0])()
@@ -58,10 +64,10 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         elif not args[0] in self.__class_name:
             print("** class doesn't exist **")
-        elif len(arg) == 1:
+        elif len(args) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(arg[0], arg[1]) in dic:
-            print(dic["{}.{}".format(arg[0], arg[1])])
+        elif "{}.{}".format(args[0], args[1]) in dic:
+            print(dic["{}.{}".format(args[0], args[1])])
         else:
             print("** no instance found **")
 
@@ -71,12 +77,12 @@ class HBNBCommand(cmd.Cmd):
         dic = storage.all()
         if not args:
             print("** class name missing **")
-        elif not arg[0] in self.__class_name:
+        elif not args[0] in self.__class_name:
             print("** class doesn't exist **")
-        elif len(arg) == 1:
+        elif len(args) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(arg[0], arg[1]) in dic:
-            dic.pop("{}.{}".format(arg[0], arg[1]))
+        elif "{}.{}".format(args[0], args[1]) in dic:
+            dic.pop("{}.{}".format(args[0], args[1]))
             storage.save()
         else:
             print("** no instance found **")
@@ -101,27 +107,27 @@ class HBNBCommand(cmd.Cmd):
         dic = storage.all()
         if not args:
             print("** class name missing **")
-        elif not arg[0] in self.__class_name:
+        elif not args[0] in self.__class_name:
             print("** class doesn't exist **")
-        elif len(arg) == 1:
+        elif len(args) == 1:
             print("** instance id missing **")
-        elif not "{}.{}".format(arg[0], arg[1]) in dic:
+        elif not "{}.{}".format(args[0], args[1]) in dic:
             print("** no instance found **")
-        elif len(arg) == 2:
+        elif len(args) == 2:
             print("** attribute name missing **")
-        elif len(arg) == 3:
+        elif len(args) == 3:
             print("** value missing **")
-        elif "{}.{}".format(arg[0], arg[1]) in dic:
-            key = dic["{}.{}".format(arg[0], arg[1])]
-            if arg[3].isdigit():
-                arg3_int = int(arg[3])
-                setattr(key, arg[2], arg3_int)
-            elif ".com" in arg[3]:
-                arg3_str = str(arg[3])
-                setattr(key, arg[2], arg3_str.strip("\""))
-            elif arg[3].isnumeric and "." in arg[3]:
-                arg3_float = float(arg[3])
-                setattr(key, arg[2], arg3_float)
+        elif "{}.{}".format(args[0], args[1]) in dic:
+            key = dic["{}.{}".format(args[0], args[1])]
+            if args[3].isdigit():
+                arg3_int = int(args[3])
+                setattr(key, args[2], arg3_int)
+            elif ".com" in args[3]:
+                arg3_str = str(args[3])
+                setattr(key, args[2], arg3_str.strip("\""))
+            elif args[3].isnumeric and "." in args[3]:
+                arg3_float = float(args[3])
+                setattr(key, args[2], arg3_float)
             key.save()
 
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,8 +2,13 @@
 """ FileStorage class module """
 import json
 from os import path
-from models.base_model import BaseModel
+from models.city import City
 from models.user import User
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.base_model import BaseModel
 
 
 class FileStorage:
