9be4a4e61031445bcea40013279f52b9c65c659d
melandres8 <melanmosta@gmail.com>
2020-06-30 21:42:10 +0000

    [unittests] adding tests for user class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,6 +3,7 @@
 import models
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -12,3 +13,32 @@ class TestUser(unittest.TestCase):
         """ Test instance of User class """
         new_instance = User()
         self.assertIsInstance(new_instance, User)
+
+    def test_id(self):
+        """ Check if user has id attribute
+            inherite from BaseModel
+        """
+        user = User()
+        self.assertTrue(hasattr(user, 'id'))
+
+    def test_id_str(self):
+        """ Check if id is string
+        """
+        user = User()
+        self.assertIsInstance(user.id, str)
+
+    def test_unique_id(self):
+        """ Checking if user has a unique ID
+        """
+        user1 = User()
+        user2 = User()
+        self.assertNotEqual(user1.id, user2.id)
+
+    def test_attr_set(self):
+        """ Cheking if user class has
+            attributes inizialice
+        """
+        self.assertEqual(User.first_name, "")
+        self.assertEqual(User.last_name, "")
+        self.assertEqual(User.email, "")
+        self.assertEqual(User.password, "")
