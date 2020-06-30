396e2de95a825ec37e7d0e1d605bff4aa14c3b0a
melandres8 <melanmosta@gmail.com>
2020-06-30 15:50:39 +0000

    tests package

---
+++ tests/test_base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Unittest for BaseModel class module """
+import models
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """ Testing class"""
+
+    def test_to_dict(self):
+        """ Checking if to_dict method is
+            returning a dictionary with all
+            keys/values of __dict__
+        """
+        new_ins = BaseModel()
+        dic = new_ins.to_dict()
+        self.assertIsInstance(dic, dict)
+        self.assertTrue(dic["__class__"] == "BaseModel")
+        for k, v in dic.items():
+            if k == "created_at" and k == "updated_at":
+                self.assertIsInstance(v, str)
+
+    def test_isinstance(self):
+        """ Test if a object is an instances
+            of BaseModel class.
+        """
+        new_instance = BaseModel()
+        self.assertIsInstance(new_instance, BaseModel)
+
+    def test_save(self):
+        """ Checking if updated_at is updated with
+            the current datetime.
+        """
+        new_instance1 = BaseModel()
+        update = new_instance1.updated_at
+        new_instance1.save()
+        self.assertNotEqual(update, new_instance1.updated_at)
+
+    def test_ids(self):
+        """ Cheking if two new instances have
+            differents ids
+        """
+        new_instance2 = BaseModel()
+        new_instance3 = BaseModel()
+        id2 = new_instance2.id
+        id3 = new_instance3.id
+        self.assertNotEqual(id2, id3)
+
+    def test_str_repr(self):
+        """ Validating string representation
+            format output.
+        """
+        formt = "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+        self.assertTrue(formt)
