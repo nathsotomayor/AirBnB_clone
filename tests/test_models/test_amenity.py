499a6e6d89ee5f54e67d87f30ca5fa48daf3c05a
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-30 18:50:06 +0000

    Add case to test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,6 +9,13 @@ class TestAmenity(unittest.TestCase):
     """ Testing class """
 
     def test_isinstance(self):
-        """ Test instance of User class """
+        """ Test instance of Amenity class """
         new_instance = Amenity()
         self.assertIsInstance(new_instance, Amenity)
+
+    def test_docstring(self):
+        """ Method to test doctring module, class and func """
+        self.assertTrue(len(amenity.__doc__) > 0)
+        self.assertTrue(len(Amenity.__doc__) > 0)
+        for funct in dir(Amenity):
+            self.assertTrue(len(funct.__doc__) > 0)
